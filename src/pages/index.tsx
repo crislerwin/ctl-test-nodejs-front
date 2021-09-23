7d69b6dc30a1e84ab99ce83b4f17272cb9c4489f
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-09-23 19:56:48 -0300

    ✅ show name and lastname when data is sent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,35 +7,33 @@ import { HeaderLogo } from '../components/HeaderLogo'
 import router from 'next/router'
 import { UserData } from '../interfaces/userData'
 const Home: React.FC = () => {
-  const onFinish = (values: UserData[]) => {
+  const onFinish = (values: UserData) => {
     api.post('/register', values).then((res) => {
       console.log(res)
-      window.alert('Your registration has been done!')
-    router.push('/Sucess')
+      window.alert(
+        `${values.first_name} ${values.last_name} your data has been registered successfully`
+      )
+      router.push('/Sucess')
     })
   }
 
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo)
   }
-  return (<>
-  <PageHeader
-    className="site-page-header"
-    title={<HeaderLogo />}
-  />
-    <Form
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <ContactForm />
-      <BillingForm />
-    </Form>
-  
-  </>
-    
+  return (
+    <>
+      <PageHeader className="site-page-header" title={<HeaderLogo />} />
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <ContactForm />
+        <BillingForm />
+      </Form>
+    </>
   )
 }
 
