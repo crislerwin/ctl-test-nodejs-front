d4feac455723c95f9e776227a74c9ab0aa67d678
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-09-24 06:03:18 -0300

    ✅  create a user data table

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,15 +1,15 @@
-import { PageHeader, Card, Col, Row } from 'antd'
+import { PageHeader, Space, Table } from 'antd'
 import { HeaderLogo } from '../components/HeaderLogo'
-import styled from "styled-components";
 
 import router from 'next/router'
 import { UserData } from '../interfaces/userData'
 import { useFetch } from '../hooks/useFetch'
 import React from 'react'
-
+const { Column, ColumnGroup } = Table
 const Sucess: React.FC = () => {
-  const {data} = useFetch<UserData[]>('/register')
- if (!data) return <h1>Loading...</h1>
+  const { data } = useFetch<UserData[]>('/register')
+  if (!data) return <h1>Loading...</h1>
+
   return (
     <>
       <PageHeader
@@ -17,91 +17,61 @@ const Sucess: React.FC = () => {
         onBack={() => router.push('/')}
         title={<HeaderLogo />}
       />
-      <Container>
-      <Row gutter={16}>
-      <Col span={8}>
-{
-  data?.map((user: UserData) => (
-   
-   
-  
-      <Card  title={`${user.first_name} ${user.last_name}`} bordered={false} style={{width: 300}}>
-     
-      <Title>Email</Title>
-      <Paragraph>{user.email}</Paragraph>
-      <Title>Phone Number</Title>
-      <Paragraph>{user.phone_number}</Paragraph>
-      <Title>Language</Title>
-      <Paragraph>{user.language}</Paragraph>
-      <Title>First Adress</Title>
-      <Paragraph>{user.first_adress}</Paragraph>
-      <Title>Second Adress</Title>
-      <Paragraph>{user.second_adress}</Paragraph>
-      <Title>Country</Title>
-      <Paragraph>{user.country}</Paragraph>
-      <Title>State</Title>
-      <Paragraph>{user.state}</Paragraph>
-      <Title>City</Title>
-      <Paragraph>{user.city}</Paragraph>
-      <Title>Zip Code</Title>
-      <Paragraph>{user.zip_code}</Paragraph>
-      <Title>Billing Zip Code</Title>
-      <Paragraph>{user.billing_zip_code}</Paragraph>
-      <Title>Billing City</Title>
-      <Paragraph>{user.billing_city}</Paragraph>
-      <Title>Billing State</Title>
-      <Paragraph>{user.billing_state}</Paragraph>
-      <Title>Primary Billing Adress</Title>
-      <Paragraph>{user.primary_billing_adress}</Paragraph>
-      <Title>Second Billing Adress</Title>
-      <Paragraph>{user.second_billing_adress}</Paragraph>
-      <Title>Other Installed Trackers?</Title>
-      <Paragraph>{user.other_installed_trackers === true ? 'Yes' : 'No'}</Paragraph>
-      <Title>Same Shipping Adress?</Title>
-      <Paragraph>{user.same_shipping_adress  === true ? 'Yes' : 'No'}</Paragraph>
-      <Title>Identify Fleet Drivers?</Title>
-      <Paragraph>{user.identify_fleet_drivers  === true ? 'Yes' : 'No'}</Paragraph>
-      <Title>Have any Fuel Cut device?</Title>
-      <Paragraph>{user.fcut_device  === true ? 'Yes' : 'No'}</Paragraph>
-      <Title>Tracker Quantity</Title>
-      <Paragraph>{user.tracker_quantity}</Paragraph>
-    </Card>
-    
+      <Space>
+        <Table dataSource={data}>
+          <ColumnGroup title="User Data">
+            <Column title="First Name" dataIndex="first_name" key="firstname" />
+            <Column title="Last Name" dataIndex="last_name" key="lastname" />
+            <Column title="Email" dataIndex="email" key="email" />
+            <Column title="Language" dataIndex="language" key="language" />
+            <Column
+              title="Adress 1"
+              dataIndex="first_adress"
+              key="firstadress"
+            />
+            <Column
+              title="Adress 2"
+              dataIndex="second_adress"
+              key="second_adress"
+            />
+            <Column title="City" dataIndex="city" key="city" />
+            <Column title="ZIP Code" dataIndex="zip_code" key="zipcode" />
 
-
-  ))}
-  </Col>
-    </Row>
-     </Container>
-  
+            <Column
+              title="Billing Adress 1"
+              dataIndex="primary_billing_adress"
+              key="billing_adress1"
+            />
+            <Column
+              title="Billing Adress 2"
+              dataIndex="second_billing_adress"
+              key="billing_adress2"
+            />
+            <Column
+              title="Billing City"
+              dataIndex="billing_city"
+              key="billing_city"
+            />
+            <Column
+              title="Billing State"
+              dataIndex="billing_state"
+              key="billing_state"
+            />
+            <Column
+              title="Billing ZIP"
+              dataIndex="billing_zip_code"
+              key="b_zipcode"
+            />
+            <Column
+              title="Trackers"
+              dataIndex="tracker_quantity"
+              key="trackerquantity"
+            />
+          </ColumnGroup>
+        </Table>
+      </Space>
     </>
   )
+}
 
-       }
- 
 export default Sucess
-
-
-
-
-const Container = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-flex-wrap: wrap;
-margin: 0 auto;
-`;
-
-const Title = styled.h1`
-color: ${p => p.theme.colors.text};
-font-size: 14px;
-font-weight: 700;
-
-
-`;
-const Paragraph = styled.p`
-color: black;
-font-size: 14px;
-font-weight: 400;
-
-`;
